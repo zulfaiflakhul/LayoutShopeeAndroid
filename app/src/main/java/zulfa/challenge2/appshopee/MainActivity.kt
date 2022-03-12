a13878c30144fb2f1304d2d68d4a3ad74f9f8d35
Zulfa <zulfaiflakhul.2019@student.uny.ac.id>
2022-03-12 19:22:15 +0700

    init

---
+++ app/src/main/java/zulfa/challenge2/appshopee/MainActivity.kt
@@ -0,0 +1,11 @@
+package zulfa.challenge2.appshopee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
